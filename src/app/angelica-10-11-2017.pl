8e96afae83ddb2a8737ca1735e40920273df80a6
Luis Felipe Sosa <luisfsosa@gmail.com>
2017-11-10 12:28:52 -0500

    Se tiene hasta que no tiene rocio

--- src/app/angelica-10-11-2017.pl
+++ src/app/angelica-10-11-2017.pl
@@ -9,12 +9,12 @@
 /* Simulation */
 
 	/*
-	 * simul(File, Format, What, Humedad, ConsumoElectricidad, ConsumoAgua, Lluvia, LC, TotC): simulation predicate
+	 * simul(File, Format, What, Humedad, ConsumoElectricidad, ConsumoAgua, LluviaPresente, LC, TotC): simulation predicate
 	 *
 	 *    File = 'console' or a filename (suffix '.csv' or '.txt' is added if none present)
 	 *    Format = 'txt' or 'csv' (format of the output)
 	 *    What = 'all' or 'best' (either list all products for a context or only the best one)
-	 *    Humedad, ConsumoElectricidad, ConsumoAgua, Lluvia: context (can be given or not, if not all are generated)
+	 *    Humedad, ConsumoElectricidad, ConsumoAgua, LluviaPresente: context (can be given or not, if not all are generated)
 	 *    LC, TotC: Claims (can be given or not, if not will TotC will be maximized)
 	 *
 	 * Example: show on the console all products for Humedad = 2 (critica) and ConsumoElectricidad = 0 (bajo) and ConsumoAgua = 0 (bajo) and Lluvia=0 (no)
@@ -22,13 +22,13 @@
 	 *
 	 * Example: create a file data.csv with the best product for each context
 	 * simul(data, csv, best, _, _, _, _, _, _).
-	 * 
+	 *
 	 * simul(console, txt, all, 2, 0, 0, 0, [1, 0, 0, _, 1], _). je veux satisfaire le premier et le dernier claims et ne pas satisfaire ni le deuxième ni le troisieme
 	 * simul(console, txt, all, 2, 0, 0, 0, _, 5). je veux satisfaire tous les claims
 	 */
 
-simul(File, Format, What, Humedad, ConsumoElectricidad, ConsumoAgua, Lluvia, LC, TotC) :-
-	findall(Product, one_product(What, Humedad, ConsumoElectricidad, ConsumoAgua, Lluvia, LC, TotC, Product), LProduct),
+simul(File, Format, What, Humedad, ConsumoElectricidad, ConsumoAgua, LluviaPresente, LC, TotC) :-
+	findall(Product, one_product(What, Humedad, ConsumoElectricidad, ConsumoAgua, LluviaPresente, LC, TotC, Product), LProduct),
 	sort(LProduct, LProduct1),
 	write_to_file(File, Format, What, LProduct1).
 
@@ -58,7 +58,7 @@ write_header(What, What, Stm, Format) :-
 	;
 	    NameLSD = ['SD1','SD2','SD3','SD4','SD5','SD6'],
 	    NameC = ['C1','C2','C3','C4','C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12']
-	),	    
+	),
 	write_line(Format, Stm, ['MedHumedad':9, 'RegHumedad':10, 'ContrAlarma':15, 'ContrSensor':8, 'ContrRociador':12,
 				 'PM', 'EE', 'TF', 'EA', 'PR', 'TotNFR',
 				 NameLSD, 'TotSD',
@@ -70,34 +70,35 @@ write_header(_, _, _, _).
 
 
 write_one(Format, What, Stm, Product) :-
-	Product = p(Humedad, ConsumoElectricidad, ConsumoAgua, Lluvia,LRC, LC, TotC, LNFR, TotNFR, LSD, TotSD, ObjValue),
-	(   g_read(cxt, [Humedad, ConsumoElectricidad,ConsumoAgua, Lluvia]) ->
+	Product = p(Humedad, ConsumoElectricidad, ConsumoAgua, LluviaPresente,LRC, LC, TotC, LNFR, TotNFR, LSD, TotSD, ObjValue),
+	(   g_read(cxt, [Humedad, ConsumoElectricidad,ConsumoAgua, LluviaPresente]) ->
 	    true
 	;
-	    g_assign(cxt, [Humedad, ConsumoElectricidad,ConsumoAgua, Lluvia]),
-	    name_of(Humedad, [normal, alert, emergency], NameHumedad),
+	    g_assign(cxt, [Humedad, ConsumoElectricidad,ConsumoAgua, LluviaPresente]),
+	    name_of(Humedad, [normal, baja, critica], NameHumedad),
 	    name_of(ConsumoElectricidad, [low, normal], NameConsumoElectricidad),
 	   	name_of(ConsumoAgua, [low, normal], NameConsumoAgua),
-	   	name_of(Lluvia, [false, true], NameLluvia),
+	   	name_of(LluviaPresente, [false, true], NameLluviaPresente),
 	    write_line(Format, Stm, []),
-	    write_line(Format, Stm, ['Context:', NameHumedad, NameConsumoElectricidad, NameConsumoAgua,NameLluvia]),
+	    write_line(Format, Stm, ['Context:', NameHumedad, NameConsumoElectricidad, NameConsumoAgua,NameLluviaPresente]),
 	    write_line(Format, Stm, []),
 	    write_header(What, all, Stm, Format)
+
 	),
-	
-	LRC = [MedirHumedadDia, _MedirHumedadHora, SPTopology, _FHTopology, DistributedProcessing, _SingleNodeProcessing],
+
+	LRC = [MedirHumedadDia, _MedirHumedadHora, Lluvia, _LineaAgua, DistributedProcessing, _SingleNodeProcessing],
 
 	LNFR = [EnergyEfficiency, ToleranciaFallos, PredictionAccuracy],
 
 	name_of(MedirHumedadDia, ['MedirHumedadHora', 'MedirHumedadDia'], NombreMH),
-	name_of(SPTopology, ['FHTopology', 'SPTopology'], NameON),
+	name_of(Lluvia, ['Lluvia', 'LineaAgua'], NombreRH),
 	name_of(DistributedProcessing, ['SingleNodeProc', 'DistributedProc'], NameCFR),
 
 	name_of(EnergyEfficiency, [--, -, =, +, ++], LevelEE),
 	name_of(ToleranciaFallos, [--, -, =, +, ++], LevelFT),
 	name_of(PredictionAccuracy, [--, -, =, +, ++], LevelPA),
-	
-	write_line(Format, Stm, [NombreMH:9, NameON:10, NameCFR:15, LevelEE:2, LevelFT:2, LevelPA:2, TotNFR:6, LSD, TotSD:5, LC, TotC:4, ObjValue:6]).
+
+	write_line(Format, Stm, [NombreMH:9, NombreRH:10, NameCFR:15, LevelEE:2, LevelFT:2, LevelPA:2, TotNFR:6, LSD, TotSD:5, LC, TotC:4, ObjValue:6]).
 
 
 write_line(txt, Stm, LElem) :-
@@ -156,18 +157,18 @@ name_of(I, L, X) :-	% act ast nth0/3, could be replaced by it
 
 
 /* Constraint Program */
-	
-one_product(What, Humedad, ConsumoElectricidad,ConsumoAgua,Lluvia, LC, TotC, Product) :-
-	set_constraint(Humedad, ConsumoElectricidad, ConsumoAgua,Lluvia, LC, TotC, LRC, LNFR, LSubNFR, TotNFR, LSD, TotSD, ObjValue),
-	fd_labeling([Humedad, ConsumoElectricidad, ConsumoAgua,Lluvia]), % if not given, try all possibilites
+
+one_product(What, Humedad, ConsumoElectricidad,ConsumoAgua,LluviaPresente, LC, TotC, Product) :-
+	set_constraint(Humedad, ConsumoElectricidad, ConsumoAgua,LluviaPresente, LC, TotC, LRC, LNFR, LSubNFR, TotNFR, LSD, TotSD, ObjValue),
+	fd_labeling([Humedad, ConsumoElectricidad, ConsumoAgua,LluviaPresente]), % if not given, try all possibilites
 	(   What = all ->
 	    fd_labeling(LRC)	% label LRC before maximization to try all possibilites
 	;
 	    true
 	),
 	fd_maximize(enumerate(LC, LRC, LNFR, LSubNFR, LSD), ObjValue),
-	Product = p(Humedad, ConsumoElectricidad,ConsumoAgua, Lluvia, LRC, LC, TotC, LNFR, TotNFR, LSD, TotSD, ObjValue).
-	
+	Product = p(Humedad, ConsumoElectricidad,ConsumoAgua, LluviaPresente, LRC, LC, TotC, LNFR, TotNFR, LSD, TotSD, ObjValue).
+
 
 
 enumerate(LC, LRC, LNFR, LSubNFR, LSD) :-
@@ -180,33 +181,33 @@ enumerate(LC, LRC, LNFR, LSubNFR, LSD) :-
 
 
 
-set_constraint(Humedad, ConsumoElectricidad,ConsumoAgua,Lluvia, LC, TotC, LRC, LNFR, LSubNFR, TotNFR, LSD, TotSD, ObjValue):-
+set_constraint(Humedad, ConsumoElectricidad,ConsumoAgua,LluviaPresente, LC, TotC, LRC, LNFR, LSubNFR, TotNFR, LSD, TotSD, ObjValue):-
 
 	fd_domain(ConsumoElectricidad, 0, 1),     %  0 = low, 1 = normal
 	fd_domain(ConsumoAgua, 0, 1),     %  0 = low, 1 = normal
-	fd_domain(Lluvia, 0, 1),     %  0 = false, 1 = true
-	fd_domain(Humedad, 0, 2),     %  0 = normal, 1 = alert, 2 = emergency
+	fd_domain(LluviaPresente, 0, 1),     %  0 = false, 1 = true
+	fd_domain(Humedad, 0, 2),     %  0 = normal, 1 = baja, 2 = Crítica
 
 	% Hard Goals (common to all product variants)
 
-	LHG = [PredictFlooding, MedirHumedad, MeasureDepth, CalculateFlowRate, OrganizeNetwork ],
+	LHG = [ControlarHumedad, MedirHumedad, RegularHumedad, MeasureDepth, CalculateFlowRate],
 	fd_domain_bool(LHG),
-	
+
 	% Reusable Components (operationalization of the Hard Goals)
-	
-	LRC = [MedirHumedadDia, MedirHumedadHora, SPTopology, FHTopology, DistributedProcessing, SingleNodeProcessing],
+
+	LRC = [MedirHumedadDia, MedirHumedadHora, Lluvia, LineaAgua, DistributedProcessing, SingleNodeProcessing],
 	fd_domain_bool(LRC),
 
 	% Non Functional Requirements
-	
+
 	LNFR = [EnergyEfficiency, ToleranciaFallos, PredictionAccuracy],
 	fd_domain(LNFR, 0, 4),
 
-	LSubNFR = [TDEnergyEfficiency, CFREnergyEfficiency, TDToleranciaFallos, ONToleranciaFallos, CFRPredictionAccuracy],
+	LSubNFR = [MHEnergyEfficiency, CFREnergyEfficiency, MHToleranciaFallos, RHToleranciaFallos, CFRPredictionAccuracy],
 	fd_domain(LSubNFR, 0, 4),
 
 	% Claims
-	
+
 	LC = [C1, C2, C3, C4, C5],
 	fd_domain_bool(LC),
 
@@ -214,25 +215,25 @@ set_constraint(Humedad, ConsumoElectricidad,ConsumoAgua,Lluvia, LC, TotC, LRC, L
 	fd_domain_bool(LSD),
 
 	% Constraints on: Hard Goals and NFR
-	
-	PredictFlooding #= 1,
 
-	PredictFlooding * 4 #= MedirHumedad + OrganizeNetwork + CalculateFlowRate + MeasureDepth,
+	ControlarHumedad #= 1,
+
+	ControlarHumedad * 4 #= MedirHumedad + RegularHumedad + CalculateFlowRate + MeasureDepth,
 
 	MedirHumedad #= MedirHumedadHora + MedirHumedadDia,
-	
-	OrganizeNetwork #= FHTopology + SPTopology,
-	
+
+	RegularHumedad #= Lluvia + LineaAgua,
+
 	CalculateFlowRate #= DistributedProcessing + SingleNodeProcessing,
 
 
 	% Constraints on Claims (as preferences)
 
-	C1 #<=> (MedirHumedadHora #==> TDEnergyEfficiency #=< 2) #/\ (MedirHumedadDia #==> TDEnergyEfficiency #>= 3),
+	C1 #<=> (MedirHumedadHora #==> MHEnergyEfficiency #=< 2) #/\ (MedirHumedadDia #==> MHEnergyEfficiency #>= 3),
 
-	C2 #<=> (MedirHumedadHora #==> TDToleranciaFallos #>= 3) #/\ (MedirHumedadDia #==> TDToleranciaFallos #=< 0),
+	C2 #<=> (MedirHumedadHora #==> MHToleranciaFallos #>= 3) #/\ (MedirHumedadDia #==> MHToleranciaFallos #=< 0),
 
-	C3 #<=> (FHTopology #==> ONToleranciaFallos #>= 4) #/\ (SPTopology #==> ONToleranciaFallos #=< 0),
+	C3 #<=> (LineaAgua #==> RHToleranciaFallos #>= 4) #/\ (Lluvia #==> RHToleranciaFallos #=< 0),
 
 	C4 #<=> (SingleNodeProcessing #==> CFREnergyEfficiency #>= 3) #/\ (DistributedProcessing #==> CFREnergyEfficiency #=< 0),
 
@@ -242,16 +243,16 @@ set_constraint(Humedad, ConsumoElectricidad,ConsumoAgua,Lluvia, LC, TotC, LRC, L
 
 	% NFR as mean of SubNFR
 
-	EnergyEfficiency #= (TDEnergyEfficiency + 2 * CFREnergyEfficiency) // 3,
-	
-	ToleranciaFallos #=< (TDToleranciaFallos + ONToleranciaFallos) // 2 + 1, % + 1 to relax a bit
-	ToleranciaFallos #=< (TDToleranciaFallos + ONToleranciaFallos),          % if both are 0 then ToleranciaFallos = 0
+	EnergyEfficiency #= (MHEnergyEfficiency + 2 * CFREnergyEfficiency) // 3,
+
+	ToleranciaFallos #=< (MHToleranciaFallos + RHToleranciaFallos) // 2 + 1, % + 1 to relax a bit
+	ToleranciaFallos #=< (MHToleranciaFallos + RHToleranciaFallos),          % if both are 0 then ToleranciaFallos = 0
 
 	PredictionAccuracy #= CFRPredictionAccuracy,
-	
+
 	% TotNFR #= EnergyEfficiency + ToleranciaFallos + PredictionAccuracy,
 	TotNFR #= EnergyEfficiency * EnergyEfficiency + ToleranciaFallos * ToleranciaFallos + PredictionAccuracy * PredictionAccuracy,
-	
+
 	% Soft Dependencies
 
 	SD1 #<=> (ConsumoElectricidad #= 0 #==> EnergyEfficiency #= 4),
@@ -260,12 +261,12 @@ set_constraint(Humedad, ConsumoElectricidad,ConsumoAgua,Lluvia, LC, TotC, LRC, L
 
 	SD3 #<=> (Lluvia #= 0 #==> EnergyEfficiency #= 4),
 
-	SD4 #<=> (Humedad #= 0 #==> EnergyEfficiency #= 4), 
+	SD4 #<=> (Humedad #= 0 #==> EnergyEfficiency #= 4),
 
-	SD5 #<=> (Humedad #= 2 #==> (ToleranciaFallos #= 4 #/\ PredictionAccuracy #= 4)), 
+	SD5 #<=> (Humedad #= 2 #==> (ToleranciaFallos #= 4 #/\ PredictionAccuracy #= 4)),
 
-	SD6 #<=> (Humedad #= 1 #==> PredictionAccuracy #= 4), 
-	
+	SD6 #<=> (Humedad #= 1 #==> PredictionAccuracy #= 4),
+
 	TotSD #= SD1 + SD2 + SD3+ SD4 +SD5 + SD6,
 
 	ObjValue #= 1000 * TotC + 100 * TotSD + TotNFR.
